2fa39f39d1474471bd0341ea56dea0cfb2cc4143
Jackal <v@njackal.com>
2023-12-09 23:48:02 -0600

    basic command done

--- commands/mainStorage/search.cmd.js
+++ commands/mainStorage/search.cmd.js
@@ -1,10 +1,30 @@
 const {SlashCommandBuilder} = require("discord.js")
+const ms = require("mainStorage/mainStorage")
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("search")
-        .setDescription("Search for an item in main storage"),
-    async execute(interaction) {
-        await interaction.reply("use /search <itemName> to search for an item")
+        .setDescription("Search for an item in main storage")
+        .addStringOption(option =>
+            option
+                .setName("item")
+                .setDescription("Name of the item you are searching for")
+                .setRequired(true)
+        ),
+    async execute(interaction) {//TODO Embeds
+        const item = interaction.options.getString("item")
+        const results = await ms.getItem(item)
+        if(results.length === 0) {
+            await interaction.reply(`No results for ${item}`)
+        } else {
+            let out = ".\n"
+            results.forEach((result) => {
+                out += result.msg
+                out += "\n"
+            })
+            await interaction.reply(out)
+        }
+
+
     }
 }
