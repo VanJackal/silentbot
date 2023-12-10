e686d8928c9e6e18aa67b40f299d61584d08e26b
Jackal <v@njackal.com>
2023-12-09 21:10:24 -0600

    Initial Command setup

---
+++ deployCommands.js
@@ -0,0 +1,47 @@
+const { REST, Routes } = require('discord.js')
+const fs = require("node:fs")
+const path = require("node:path")
+const CLIENT_ID = process.env.CLIENTID
+const TOKEN = process.env.TOKEN
+
+const commands = []
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    // Grab all the command files from the commands directory you created earlier
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.cmd.js'));
+    // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            commands.push(command.data.toJSON());
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+
+const rest = new REST().setToken(TOKEN);
+
+//DEPLOY
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+        // The put method is used to fully refresh all commands in the guild with the current set
+        const data = await rest.put(
+            //Routes.applicationCommands(CLIENT_ID),
+            Routes.applicationGuildCommands(CLIENT_ID,"667029987479650324"),
+            { body: commands },
+        );
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } catch (error) {
+        // And of course, make sure you catch and log any errors!
+        console.error(error);
+    }
+})();
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("node:fs")
 const path = require('node:path')
 const {Client, Collection, Events, GatewayIntentBits} = require("discord.js")
+require("./deployCommands")// auto deploy
 
 const TOKEN = process.env.TOKEN
 
